62f4789ee728d27648763df2bea12931d0f05cfa
Saima Rahman <rahman.saima92@gmail.com>
2019-11-08 20:17:17 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+exit
 # Reverse Each Word
 
 ## Learning Goals
---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(string) 
+  i = 0 
+  while i < string.length do
+    word = string[i]
+    p word
+    i+=1
+  end 
+end 
