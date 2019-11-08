a9f7581677cfacc2cc782f27496b621c628b848a
Saima Rahman <rahman.saima92@gmail.com>
2019-11-08 20:48:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ def reverse_each_word(string)
   result=  string.split(" ")
   reversed =  result.each{|ele| 
   reversed_ele =  ele.reverse
-  reversed_ele
+  p reversed_ele
   }
  
   
-  return reversed
+  
 end 
