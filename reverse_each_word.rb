dc573b9c93467c202485576255980d51808331ae
Saima Rahman <rahman.saima92@gmail.com>
2019-11-08 20:59:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,13 @@
-def reverse_each_word(string) 
-  result=  string.split(" ")
-  new_str = []
-    reversed =  result.each{|ele| 
-    ele.reverse
-    new_str.push(ele.reverse)
-  }
+# def reverse_each_word(string) 
+#   result=  string.split(" ")
+#   new_str = []
+#     reversed =  result.each{|ele| 
+#     ele.reverse
+#     new_str.push(ele.reverse)
+#   }
  
-  new_str.join(' ')
-end 
+#   new_str.join(' ')
+# end 
 
 # def reverse_each_word(string) 
 #   result=  string.split(" ")
